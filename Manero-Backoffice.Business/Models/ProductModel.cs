7b66debfb5c60f67a8403c5ef7b310442728c0be
jaghetermarkus <143387643+jaghetermarkus@users.noreply.github.com>
2024-05-15 21:45:15 +0200

    Update in product.razor + remove login.css files

--- Manero-Backoffice.Business/Models/ProductModel.cs
+++ Manero-Backoffice.Business/Models/ProductModel.cs
@@ -23,24 +23,4 @@ public class ProductModel
         public string SubCategory { get; set; } = null!; 
         public string Format { get; set; } = null!; 
     }
-
-
-
-// {
-//     public string Id { get; set; } = null!;
-//     public string Title { get; set; } = null!;
-//     public string Author { get; set; } = null!;
-//     public string? ShortDescription { get; set; }
-//     public string? LongDescription { get; set; }
-//     public string Language { get; set; } = null!;
-//     public string? Pages { get; set; }
-//     public string? PublishDate { get; set; }
-//     public string? Publisher { get; set; }
-//     public string? ISBN { get; set; }
-//     public List<string> ImageUrls { get; set; } = new List<string>();
-
-//     public List<CategoryModel> Categories { get; set; } = new List<CategoryModel>();
-//     public List<FormatModel> Formats { get; set; } = new List<FormatModel>();
-//     public List<SubCategoryModel> SubCategories { get; set; } = new List<SubCategoryModel>();
-// }
-
+    
