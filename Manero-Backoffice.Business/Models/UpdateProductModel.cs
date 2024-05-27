ca33a913325203c46cef109b2b7dc89df5085e35
jaghetermarkus <143387643+jaghetermarkus@users.noreply.github.com>
2024-05-27 11:45:02 +0200

    Update+AddProduct fixed

---
+++ Manero-Backoffice.Business/Models/UpdateProductModel.cs
@@ -0,0 +1,25 @@
+﻿namespace Manero_Backoffice.Business.Models;
+
+public class UpdateProductModel
+{
+        public string Id { get; set; } = null!;
+        public string Title { get; set; } = null!;
+        public string Author { get; set; } = null!;
+        public string Price { get; set; } = null!;
+        public string DiscountPrice { get; set; } = null!;
+        public string ShortDescription { get; set; } = null!;
+        public string LongDescription { get; set; } = null!;
+        public string Language { get; set; } = null!;
+        public string Pages { get; set; } = null!;
+        public DateTime PublishDate { get; set; }
+        public string Publisher { get; set; } = null!;
+        public string ISBN { get; set; } = null!;
+        public string ImageUrl { get; set; } = null!; 
+        public bool OnSale { get; set; }
+        public bool BestSeller { get; set; }
+        public bool IsFavorite { get; set; }
+        public bool FeaturedProduct { get; set; }
+        public string CategoryName { get; set; } = null!; 
+        public string SubCategoryName { get; set; } = null!; 
+        public string FormatName { get; set; } = null!; 
+}
--- Manero-Backoffice/Manero-Backoffice/Services/ProductService.cs
+++ Manero-Backoffice/Manero-Backoffice/Services/ProductService.cs
@@ -22,13 +22,78 @@ public class ProductService(HttpClient http, IConfiguration configuration)
             return [];
         }
     }
+    public async Task<List<ProductModel>> GetProductsByFilter(string category, string subCategory, string format, bool onSale, bool isFavorite, bool Bestseller, bool featuredProduct)
+    {
+        try
+        {
+            var query = _configuration.GetValue<string>("ApiStrings:GetAllProductsByFilter");
 
-    public async Task<bool> CreateProduct(ProductRegistrationForm product)
+            if (!string.IsNullOrEmpty(category))
+                query += $"&category={category}";
+            if (!string.IsNullOrEmpty(subCategory))
+                query += $"&subCategory={subCategory}";
+            if (!string.IsNullOrEmpty(format))
+                query += $"&format={format}";
+            if (onSale)
+                query += $"&onSale={onSale}";
+            if (isFavorite)
+                query += $"&isFavorite={isFavorite}";
+            if (Bestseller)
+                query += $"&Bestseller={Bestseller}";
+            if (featuredProduct)
+                query += $"&featuredProduct={featuredProduct}";
+
+            var result = await Http.GetFromJsonAsync<List<ProductModel>>(query);
+            return result ?? [];
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+            return [];
+        }
+    }
+
+    public async Task<UpdateProductModel> GetProductById(string productId)
+    {
+        try
+        {
+            var query = _configuration.GetValue<string>("ApiStrings:GetProductByFilter");
+            query += $"&id={productId}";
+            // https://maneroproductsfunction.azurewebsites.net/api/SortProduct?code=9mfmPoUUxcBnyQ-CyFMAlX3U_ldlg9X-kEBpdyF3fyPLAzFuIV8aww%3D%3D?id=3d89ec1f-fd51-4279-88e8-9a425f3edc5b
+
+            // var result = await Http.GetFromJsonAsync<ProductModel>(query);
+            // if (result != null)
+            //     return result;
+            var result = await Http.GetFromJsonAsync<List<UpdateProductModel>>(query);
+            if (result != null && result.Count > 0)
+                return result[0];
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+            return null!;
+    }
+
+    public async Task<bool> CreateProductAsync(ProductRegistrationForm product)
     {
         try
         {
             var url = _configuration.GetValue<string>("ApiStrings:CreateProduct");
             var respons = await Http.PostAsJsonAsync(url, product);
+            if (respons.IsSuccessStatusCode)
+                return true;
+        }
+        catch (System.Exception) { }
+        return false!;
+    }
+
+    public async Task<bool> UpdateProductAsync(UpdateProductModel product)
+    {
+        try
+        {
+            var url = _configuration.GetValue<string>("ApiStrings:UpdateProduct");
+            var respons = await Http.PutAsJsonAsync(url, product);
             if (respons.IsSuccessStatusCode)
                 return true;
         }
